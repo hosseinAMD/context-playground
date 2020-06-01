895779ccb40d051720d3069084f6bafdac8db263
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 13:11:07 +0430

    console warnings cleaned

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { AuthContext } from "../contexts/AuthContext";
 
