23135fcbfeb3ffb218cadede0e42275343b7aa5a
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 12:03:13 +0430

    auth context consumer added to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import { AuthContext } from "../contexts/AuthContext";
 
 class Navbar extends Component {
-  static contextType = ThemeContext;
   render() {
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, dark, light } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ background: theme.ui, color: theme.text }}>
-              <h3>FavP</h3>
-              <ul>
-                <li className="navbar-item">Home</li>
-                <li className="navbar-item">Teams</li>
-                <li className="navbar-item">Players</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isAuthenticated, toggleAuth } = authContext;
+              const { isLightTheme, dark, light } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.text }}>
+                  <h3>FavP</h3>
+                  <div style={{ cursor: "pointer" }} onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged in" : "Logged out"}
+                  </div>
+                  <ul>
+                    <li className="navbar-item">Home</li>
+                    <li className="navbar-item">Teams</li>
+                    <li className="navbar-item">Players</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
