a44f81d1f21891dabb6e021974ec5caec751d87d
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 13:10:33 +0430

    toggle theme component converted to function

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,12 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { toggleTheme } = this.context;
-    return <button onClick={toggleTheme}>Toggle the Theme</button>;
-  }
-}
+const ThemeToggle = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+  return <button onClick={toggleTheme}>Toggle the Theme</button>;
+};
 
 export default ThemeToggle;
