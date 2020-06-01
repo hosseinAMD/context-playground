e2bb2bb5f0069e9de1f886fe85b585aab18917f0
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 13:09:36 +0430

    navbar component converted to function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,24 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 import { AuthContext } from "../contexts/AuthContext";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { isLightTheme, dark, light } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              return (
-                <nav style={{ background: theme.ui, color: theme.text }}>
-                  <h3>FavP</h3>
-                  <div style={{ cursor: "pointer" }} onClick={toggleAuth}>
-                    {isAuthenticated ? "Logged in" : "Logged out"}
-                  </div>
-                  <ul>
-                    <li className="navbar-item">Home</li>
-                    <li className="navbar-item">Teams</li>
-                    <li className="navbar-item">Players</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+const Navbar = () => {
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.text }}>
+      <h3>FavP</h3>
+      <div style={{ cursor: "pointer" }} onClick={toggleAuth}>
+        {isAuthenticated ? "Logged in" : "Logged out"}
+      </div>
+      <ul>
+        <li className="navbar-item">Home</li>
+        <li className="navbar-item">Teams</li>
+        <li className="navbar-item">Players</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
