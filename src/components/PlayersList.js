033ac0018610e9177ca94a8bde7f380580507c7f
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 13:03:40 +0430

    players list component converted to function

--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -1,31 +1,22 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class PlayersList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, dark, light } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <div
-              className="players-list"
-              style={{ background: theme.bg, color: theme.text }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>Christiano Ronaldo</li>
-                <li style={{ background: theme.ui }}>Paulo Maldini</li>
-                <li style={{ background: theme.ui }}>Andrea Pirlo</li>
-                <li style={{ background: theme.ui }}>Cludio Marchisio</li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const PlayersList = () => {
+  const { isLightTheme, dark, light } = useContext(ThemeContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <div
+      className="players-list"
+      style={{ background: theme.bg, color: theme.text }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>Christiano Ronaldo</li>
+        <li style={{ background: theme.ui }}>Paulo Maldini</li>
+        <li style={{ background: theme.ui }}>Andrea Pirlo</li>
+        <li style={{ background: theme.ui }}>Cludio Marchisio</li>
+      </ul>
+    </div>
+  );
+};
 
 export default PlayersList;
