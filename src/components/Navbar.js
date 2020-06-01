a2b6ee0861976b6920fbdb544fd8bc2304f2dfab
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-01 11:05:11 +0430

    consume theme context in childrens by consumer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,23 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class Navbar extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isLightTheme, dark, light } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <nav style={{ background: theme.ui, color: theme.text }}>
-        <h3>FavP</h3>
-        <ul>
-          <li className="navbar-item">Home</li>
-          <li className="navbar-item">Teams</li>
-          <li className="navbar-item">Players</li>
-        </ul>
-      </nav>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, dark, light } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <nav style={{ background: theme.ui, color: theme.text }}>
+              <h3>FavP</h3>
+              <ul>
+                <li className="navbar-item">Home</li>
+                <li className="navbar-item">Teams</li>
+                <li className="navbar-item">Players</li>
+              </ul>
+            </nav>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -4,20 +4,26 @@ import { ThemeContext } from "../contexts/ThemeContext";
 class PlayersList extends Component {
   static contextType = ThemeContext;
   render() {
-    const { isLightTheme, dark, light } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div
-        className="players-list"
-        style={{ background: theme.bg, color: theme.text }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>Christiano Ronaldo</li>
-          <li style={{ background: theme.ui }}>Paulo Maldini</li>
-          <li style={{ background: theme.ui }}>Andrea Pirlo</li>
-          <li style={{ background: theme.ui }}>Cludio Marchisio</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isLightTheme, dark, light } = context;
+          const theme = isLightTheme ? light : dark;
+          return (
+            <div
+              className="players-list"
+              style={{ background: theme.bg, color: theme.text }}
+            >
+              <ul>
+                <li style={{ background: theme.ui }}>Christiano Ronaldo</li>
+                <li style={{ background: theme.ui }}>Paulo Maldini</li>
+                <li style={{ background: theme.ui }}>Andrea Pirlo</li>
+                <li style={{ background: theme.ui }}>Cludio Marchisio</li>
+              </ul>
+            </div>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
