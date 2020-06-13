7c2f761716b58aa62e1e799eae28f5672384523c
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2020-06-13 16:14:21 +0430

    rate context synced with localStorage

--- src/contexts/RateContext.js
+++ src/contexts/RateContext.js
@@ -1,10 +1,18 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { rateReducer } from "../reducers/rateReducer";
 
 export const RateContext = createContext();
 
 const RateContextProvider = (props) => {
-  const [likes, dispatch] = useReducer(rateReducer, 0);
+  const [likes, dispatch] = useReducer(rateReducer, 0, () => {
+    const localLikes = localStorage.getItem("likes");
+    return localLikes ? JSON.parse(localLikes).likes : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("likes", JSON.stringify({ likes }));
+  }, [likes]);
+
   return (
     <RateContext.Provider value={{ likes, dispatch }}>
       {props.children}
